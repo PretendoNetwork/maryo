8648c6e2d125dc98097614d81d215d113720be56
Superwhiskers <kspy360@rocketmail.com>
2018-03-13 18:48:20 -0500

    fix a bug regarding autoconf and adding the Pretendo servers to the config

---
+++ utils.go
@@ -0,0 +1,16 @@
+/*
+
+maryo/utils.go
+
+contains some utilities for things like JSON
+
+written by superwhiskers, licensed under gnu gplv3.
+if you want a copy, go to http://www.gnu.org/licenses/
+
+*/
+
+package main
+
+// internals
+
+// stuff might go here
